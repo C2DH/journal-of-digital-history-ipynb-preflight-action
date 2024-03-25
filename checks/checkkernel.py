38482634aee638f5711de1e855fca2977b140e38
Yaroslav <yaro3245@gmail.com>
2024-03-25 11:09:42 +0100

    Update checkkernel.py

--- checks/checkkernel.py
+++ checks/checkkernel.py
@@ -2,7 +2,7 @@ import os
 import json
 
 def checkkernel(contents, output=None):
-    std_output = "Kernel checks output:\n"
+    std_output = ""
     md_output="## Kernel Checks: \n\n"
 
     print("::debug::checkkernel")
@@ -14,18 +14,18 @@ def checkkernel(contents, output=None):
     kernelJSON = json.loads(json.dumps(kernel))
 
     if(kernelJSON["name"]=="R"):
-        md_output+=f"#### Programming Language is R; version - "+kernelJSON["version"]+"\n"
-        std_output+=f"\nLanguage is R"
+        md_output+="#### Programming Language is R; version - "+kernelJSON["version"]+"\n"
+        std_output+=f"Language is R\n"
 
     ## if language is not R
     else:
-        std_output+=f"\nLanguage is NOT R"
+        std_output+=f"Language is NOT R\n"
 
         # check if runtime.txt exists
         runtimeFileExists = os.path.isfile('./runtime.txt')
 
         if(runtimeFileExists==False):
-            std_output+=f"\nruntime.txt file is missing"
+            std_output+=f"runtime.txt file is missing\n"
             md_output+=f"> [!CAUTION]\n"
             md_output+=f"> ERROR: **runtime.txt** is MISSING\n"
             return [md_output, std_output]
@@ -40,12 +40,13 @@ def checkkernel(contents, output=None):
         read_content = runtime_txt.read()
         if(read_content==notebook_language):
             
-            std_output+=f"\nPython versions match"
+            std_output+=f"Python versions match\n"
             md_output+=f"#### SUCCESS: Python versions match; {read_content} -> {notebook_language}\n"
         else:
-            std_output+=f"\nPython versions don't match"
+            std_output+=f"Python versions don't match\n"
             md_output+=f"> [!CAUTION]\n"
             md_output+=f"> ERROR: Different Python versions. {read_content} expected; {notebook_language} received\n"
     
+    std_output+='end of kernel checks'
     return md_output, std_output
     
