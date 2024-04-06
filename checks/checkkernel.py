328ea597081888edbd7e4f584ebbe3b398a783ed
Yaroslav <yaro3245@gmail.com>
2024-04-06 23:46:39 +0200

    checkkernel mistype fix

--- checks/checkkernel.py
+++ checks/checkkernel.py
@@ -1,9 +1,10 @@
 import os
 import json
 
+
 def checkkernel(contents, output=None):
     std_output = ""
-    md_output="## Kernel Checks: \n\n"
+    md_output = "## Kernel Checks: \n\n"
 
     print("::debug::checkkernel")
 
@@ -13,8 +14,8 @@ def checkkernel(contents, output=None):
     # converting to JSON
     kernelJSON = json.loads(json.dumps(kernel))
 
-    if(kernelJSON["name"]=="R"):
-        md_output+=f"Programming language is **R ({kernelJSON['version']})**\n"
+    if kernelJSON["name"] == "R":
+        md_output += f"Programming language is **R ({kernelJSON['version']})**\n"
         # std_output+="Language is R\n"
 
     ## if language is not R
@@ -22,31 +23,32 @@ def checkkernel(contents, output=None):
         # std_output+="Language is NOT R"
 
         # check if runtime.txt exists
-        runtimeFileExists = os.path.isfile('./runtime.txt')
+        runtimeFileExists = os.path.isfile("./runtime.txt")
 
-        if(runtimeFileExists==False):
+        if runtimeFileExists == False:
             # std_output+="runtime.txt file is missing\n"
-            md_output+=f"> [!CAUTION]\n"
-            md_output+=f"> Error: **runtime.txt** is missing\n"
+            md_output += f"> [!CAUTION]\n"
+            md_output += f"> Error: **runtime.txt** is missing\n"
             return [md_output, std_output]
-        
+
         # if runtime.txt exists, then check if the language from notebook matches the one in runtime.txt
-        
-        notebook_language = kernelJSON["name"]+"-"+kernelJSON["version"]
-        
+
+        notebook_language = kernelJSON["name"] + "-" + kernelJSON["version"]
+
         runtime_txt = open("./runtime.txt", "r")
 
         # read the file
         read_content = runtime_txt.read()
-        if(read_content==notebook_language):
-            
+        if read_content == notebook_language:
+
             # std_output+="Python versions match\n"
-            md_output+=f"Python versions match :white_check_mark:\n"
+            md_output += f"Python versions match :white_check_mark:\n"
         else:
             # std_output+="Python versions dont match"
-            md_output+=f"> [!CAUTION]\n > Error: Python versions don't match. The notebook is using **{notebook_language}**, when **{read_content}** is required.\n"
-            md_output+=f"> [!TIP]\n > Try changing **requirements.txt** to resolve the error above."
-    
+            md_output += f"> [!CAUTION]\n > Error: Python versions don't match. The notebook is using **{notebook_language}**, when **{read_content}** is required.\n"
+            md_output += (
+                f"> [!TIP]\n > Try changing **runtime.txt** to resolve the error above."
+            )
+
     # std_output+=f"end of kernel checks"
     return md_output, std_output
-    
