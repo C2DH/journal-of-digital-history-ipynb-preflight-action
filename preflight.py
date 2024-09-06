93059e3278fae68618899472f49708ffb1dc408a
Yaroslav <73663808+memerchik@users.noreply.github.com>
2024-09-06 14:24:48 +0200

    first paragraph update
    #39

--- preflight.py
+++ preflight.py
@@ -9,7 +9,13 @@ from urllib.parse import quote
 
 BASE_URL = "https://journalofdigitalhistory.org/en/notebook-viewer/"
 
-FIRST_PARAGRAPH = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+f = open("./article-check-script/config.json")
+
+config_file = json.load(f)
+
+f.close()
+
+FIRST_PARAGRAPH = config_file["first_paragraph"]
 
 
 def encode_notebook_url(url):
